844d634ed559ee2d9c6544add2c7a1ee30999c2a
Hector <gt12930@GMAIL.com>
2022-09-05 20:45:39 -0500

    fix: estilos de productos y header

--- js/renderProducts.js
+++ js/renderProducts.js
@@ -1,5 +1,4 @@
 export const renderProducts = (data) => {
-
   let products = document.getElementsByClassName("products")[0];
   let div = document.createElement("div");
   div.classList.add("product-container");
@@ -11,19 +10,15 @@ export const renderProducts = (data) => {
           ? ` 
   
           <div class="product">
-          <img src=${item.url_image} alt="" class="image skeleton">
-          <div class="product-info">
-          <label class="name">${item.name}</label>
-          ${
-            item.discount > 0
-              ? `<p class="discount"> ${
-                  item.discount > 0 ? "-" + item.discount + "%" : ""
-                } </p> `
-              : ""
-          }
-          <label class="price"> $${item.price}</label>
-          </div>    
-           </div>`
+            <div class="product-image">
+            <img src=${item.url_image} alt="" class="image skeleton">
+            </div>
+            <div class="product-info">
+              <label class="name">${item.name}</label>
+
+              <label class="price"> $${item.price}</label>
+            </div>    
+          </div>`
           : ""
       }
         `;
@@ -36,33 +31,26 @@ const price_asc = document.querySelector(".asc");
 const price_desc = document.querySelector(".desc");
 const discount = document.querySelector(".disc");
 
-let term = ""
+let term = "";
 export const orderBy = async (e) => {
-
-
-  if(e.target.classList[0] === "input-search"){
-
-    term= e.target.value
+  if (e.target.classList[0] === "input-search") {
+    term = e.target.value;
   }
-  if(term === ""){
+  if (term === "") {
     let products = document.getElementsByClassName("products")[0];
     const response = await fetch(
       `https://bsale2.herokuapp.com/api/v1/products?sortBy=${e.target.value}`
     );
     const data = await response.json();
-  
-  
+
     if (data?.content.length > 0) {
       products.innerHTML = "";
       renderProducts(data);
     }
   }
- 
 };
-document
-  .querySelector(".input-search")
-  .addEventListener("keyup", orderBy);
+document.querySelector(".input-search").addEventListener("keyup", orderBy);
 
-price_asc.addEventListener("click", orderBy,false);
-price_desc.addEventListener("click", orderBy,false);
-discount.addEventListener("click", orderBy,false);
+price_asc.addEventListener("click", orderBy, false);
+price_desc.addEventListener("click", orderBy, false);
+discount.addEventListener("click", orderBy, false);
